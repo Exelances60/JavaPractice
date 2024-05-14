dcd03411d8ffeb9826b40af7a94189ce971c5adc
Enes Çelik <enescelik6012@gmail.com>
2024-05-14 21:49:54 +0300

    Abstrac example

--- PolymorphismOverriding/src/BaseKrediManager.java
+++ PolymorphismOverriding/src/BaseKrediManager.java
@@ -1,5 +1,5 @@
 public class BaseKrediManager {
-    public double hesapla(double tutar) {
+    public  double hesapla(double tutar) { // final ile override edilmesini engelleyebiliriz.
         return tutar * 1.18;
     }
 }
